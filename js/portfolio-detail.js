15195181fab3240cc1dd9a13db8142d0a6419617
José Aponte <japonte@gr3000.com>
2016-07-21 14:07:20 -0400

    Se realizó una pequeña separación entre los elementos del Portafolio para mejor visualización. Se cambió el color del TextBox al mismo color del background de la sección "Mi Trabajo" para ocultar los bordes sobresalientes. Se ajustó el tamaño de la fuente en el Menú Principal adaptada a los distintos tamaños de visualización. se agregó más contenido a los elementos del portafolio. Se mejoró la transición entre el conjunto de elementos del portafolio y el contenido de un elemento en específico.

--- js/portfolio-detail.js
+++ js/portfolio-detail.js
@@ -1,23 +1,30 @@
 $('#url-notas_del_nerd').click(function(){
-  $('#portfolio-container').hide("slow");
-  $.get("portfolio/notas-del-nerd.html",function(data){
-            $("#portfolio-detail").append(data);
-  });
+    $('#portfolio-container').hide("slow",function(){
+          $.get("portfolio/notas-del-nerd.html",function(data){
+                $("#portfolio-detail").append(data);
+                $("#portfolio-detail").show("slow");
+          });
+    });
   location.href = "#portfolio";
 });
 
 $('#url-integria').click(function(){
-  $('#portfolio-container').hide("slow");
-  $.get("portfolio/integria.html",function(data){
-            $("#portfolio-detail").append(data);
+  $('#portfolio-container').hide("slow",function(){
+    $.get("portfolio/integria.html",function(data){
+          $("#portfolio-detail").append(data);
+          $("#portfolio-detail").show("slow");
+    });
   });
   location.href = "#portfolio";
 });
 
 $('#url-receta_salud').click(function(){
-  $('#portfolio-container').hide("slow");
-  $.get("portfolio/receta-salud.html",function(data){
-            $("#portfolio-detail").append(data);
-  });
+    $('#portfolio-container').hide("slow",function(){
+        $.get("portfolio/receta-salud.html",function(data){
+              $("#portfolio-detail").append(data);
+              $("#portfolio-detail").show("slow");
+        });
+
+    });
   location.href = "#portfolio";
 });
